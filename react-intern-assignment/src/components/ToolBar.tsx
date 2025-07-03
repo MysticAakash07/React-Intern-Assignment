4ac5c1b30d2591935d8214a3f26a8248da1aae3e
MysticAakash07 <rahulsagar460@gmail.com>
2025-07-03 13:06:24 +0530

    feat: Implemented onClick logic to ToolBar

--- react-intern-assignment/src/components/ToolBar.tsx
+++ react-intern-assignment/src/components/ToolBar.tsx
@@ -9,46 +9,81 @@ import Arrow_Split from "../assets/Arrow _Split.svg";
 
 const Toolbar = () => {
 	return (
-		<div className="flex gap-2 px-3 py-1.5 justify-between border-b border-[#eeeeee] text-sm ">
+		<div className="flex gap-2 px-3 py-1.5 justify-between border-b border-[#eeeeee] text-sm">
 			{/* Left */}
 			<div className="flex items-center gap-5">
-				<button className="flex items-center justify-center gap-1 border-r-2 border-gray-300 px-3 py-1">
+				<button
+					className="flex items-center justify-center gap-1 border-r-2 border-gray-300 px-3 py-1"
+					onClick={() => console.log("Expanding toolbar")}
+				>
 					<span>Tool bar</span>
 					<BsChevronDoubleRight />
 				</button>
-				<button className="flex items-center justify-center gap-1">
+
+				<button
+					className="flex items-center justify-center gap-1"
+					onClick={() => console.log("Hiding fields")}
+				>
 					<VscEyeClosed className="text-lg" />
 					<span>Hide fields</span>
 				</button>
-				<button className="flex items-center justify-center gap-1">
+
+				<button
+					className="flex items-center justify-center gap-1"
+					onClick={() => console.log("Sorting triggered")}
+				>
 					<HiOutlineArrowsUpDown className="text-lg" />
 					<span>Sort</span>
 				</button>
-				<button className="flex items-center justify-center gap-1">
+
+				<button
+					className="flex items-center justify-center gap-1"
+					onClick={() => console.log("Filtering triggered")}
+				>
 					<BsFilter className="text-lg" />
 					<span>Filter</span>
 				</button>
-				<button className="flex items-center justify-center gap-1 content-center">
+
+				<button
+					className="flex items-center justify-center gap-1 content-center"
+					onClick={() => console.log("Changing to Cell view")}
+				>
 					<TbArrowAutofitHeight className="text-lg" />
 					<span>Cell view</span>
 				</button>
 			</div>
+
 			{/* Right */}
-			<div className="flex items-center gap-2 ">
-				<button className=" border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2 text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in">
+			<div className="flex items-center gap-2">
+				<button
+					className="border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2 text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in"
+					onClick={() => console.log("Importing data")}
+				>
 					<LiaDownloadSolid className="text-lg" />
 					<span>Import</span>
 				</button>
-				<button className="border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2  text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in">
+
+				<button
+					className="border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2 text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in"
+					onClick={() => console.log("Exporting data")}
+				>
 					<LiaDownloadSolid className="rotate-180 text-lg" />
 					<span>Export</span>
 				</button>
-				<button className="border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2  text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in">
+
+				<button
+					className="border border-gray-200 flex items-center justify-center gap-1 py-1.75 rounded-md px-2 text-gray-600 hover:bg-[#4B6A4F] hover:text-white duration-200 ease-in"
+					onClick={() => console.log("Sharing spreadsheet")}
+				>
 					<FaRegShareFromSquare className="text-lg" />
 					<span>Share</span>
 				</button>
-				<button className="bg-[#4B6A4F] text-white flex items-center justify-center gap-1 py-1.75 rounded-md px-5">
-					<img src={Arrow_Split} alt="" />
+
+				<button
+					className="bg-[#4B6A4F] text-white flex items-center justify-center gap-1 py-1.75 rounded-md px-5"
+					onClick={() => console.log("New action started")}
+				>
+					<img src={Arrow_Split} alt="New Action" />
 					<span>New Action</span>
 				</button>
 			</div>
